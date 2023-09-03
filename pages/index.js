10f170b898bf5bb65a24c320d5550168b3b3e94c
ANURAGTIGGA <a.aront003@gmail.com>
2023-09-04 00:18:36 +0530

    adding next js pages

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import Link from 'next/Link';
+import Date from '../components/date';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+import { getSortedPostsData } from '../lib/posts';
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I am a software developer at Zeta.</p>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+      </section>
+      </section>
+    </Layout>
+  );
+}
